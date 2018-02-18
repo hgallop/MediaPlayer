780794d4377cac17ab064f69e8dcb26c599b1913
Heather Gallop <doyouseeitmyway@gmail.com>
2018-02-18 10:23:20 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/mediaplayer/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.android.mediaplayer;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.bensound_ukulele);
+
+    }
+
+    public void playAudio(View view){
+        mediaPlayer.start();
+    }
+
+    public void pauseAudio(View view){
+        mediaPlayer.pause();
+    }
+
+    public void stopAudio(View view){
+        mediaPlayer.stop();
+        mediaPlayer.release();
+        mediaPlayer =  mediaPlayer.create(this, R.raw.bensound_ukulele);
+    }
+
+}
