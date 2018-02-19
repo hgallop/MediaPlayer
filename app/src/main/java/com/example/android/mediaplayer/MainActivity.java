4959b9d11e01688eab3925b7d5b98ca967540d1c
Heather Gallop <doyouseeitmyway@gmail.com>
2018-02-19 12:12:46 -0500

    Add override method to release media player if app is stopped.

--- app/src/main/java/com/example/android/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/android/mediaplayer/MainActivity.java
@@ -4,6 +4,7 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +19,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void playAudio(View view){
+    public void playAudio(View view) {
         mediaPlayer.start();
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                Toast.makeText(MainActivity.this, "I'm Done", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void pauseAudio(View view){
@@ -30,6 +37,14 @@ public class MainActivity extends AppCompatActivity {
         mediaPlayer.stop();
         mediaPlayer.release();
         mediaPlayer =  mediaPlayer.create(this, R.raw.bensound_ukulele);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mediaPlayer != null){
+            mediaPlayer.release();
+        }
     }
 
 }
